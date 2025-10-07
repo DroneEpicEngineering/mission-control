a6300d654dc96db4971b2042b6717223e4fba162
Karolina Kozubik <k4aro0@gmail.com>
2025-10-07 19:36:52 +0200

    feat: publish vehicle commands

--- flight_control/flight_control/offboard_control_node.py
+++ flight_control/flight_control/offboard_control_node.py
@@ -1,9 +1,7 @@
 import rclpy
 from rclpy.node import Node
 
-from px4_msgs.msg import (
-    OffboardControlMode,
-)
+from px4_msgs.msg import OffboardControlMode, VehicleCommand
 
 from utils.qos_profiles import PX4_PROFILE
 
@@ -24,16 +22,46 @@ class OffboardControl(Node, metaclass=SingletonMeta):
     def __init__(self) -> None:
         super().__init__("offboard_control")
 
+        self._heartbeat_timer = self.create_timer(0.2, self.__heartbeat_timer_cb)
+        self._offboard_setpoint_counter = 0
+
         self._offboard_control_mode_pub = self.create_publisher(
             OffboardControlMode, "fmu/in/offboard_control_mode", PX4_PROFILE
         )
 
-        self._heartbeat_timer = self.create_timer(0.2, self.__heartbeat_timer_cb)
-        self._offboard_setpoint_counter = 0
+        self._vehicle_command_pub = self.create_publisher(
+            VehicleCommand, "fmu/in/vehicle_command", PX4_PROFILE
+        )
 
     @property
     def is_ready(self) -> bool:
         return self._offboard_setpoint_counter > self.OFFBOARD_SETPOINT_THRESHOLD
+
+    def arm(self) -> None:
+        self.__publish_vehicle_command(
+            VehicleCommand.VEHICLE_CMD_COMPONENT_ARM_DISARM, 1.0
+        )
+
+    def disarm(self) -> None:
+        self.__publish_vehicle_command(
+            VehicleCommand.VEHICLE_CMD_COMPONENT_ARM_DISARM, 0.0
+        )
+
+    def land(self) -> None:
+        self.__publish_vehicle_command(VehicleCommand.VEHICLE_CMD_NAV_LAND)
+
+    def return_to_launch(self) -> None:
+        self.__publish_vehicle_command(VehicleCommand.VEHICLE_CMD_NAV_RETURN_TO_LAUNCH)
+
+    def set_offboard_mode(self) -> None:
+        self.__publish_vehicle_command(
+            VehicleCommand.VEHICLE_CMD_DO_SET_MODE, param1=1.0, param2=6.0
+        )
+
+    def set_hold_mode(self) -> None:
+        self.__publish_vehicle_command(
+            VehicleCommand.VEHICLE_CMD_DO_SET_MODE, param1=1.0, param2=2.0
+        )
 
     def __heartbeat_timer_cb(self) -> None:
         self.__publish_offboard_control_mode()
@@ -50,6 +78,27 @@ class OffboardControl(Node, metaclass=SingletonMeta):
         msg.thrust_and_torque = False
         msg.direct_actuator = False
         self._offboard_control_mode_pub.publish(msg)
+
+    def __publish_vehicle_command(self, command: int, **params) -> None:
+        msg = VehicleCommand()
+        msg.command = command
+        msg.param1 = params["param1"] or 0
+        msg.param2 = params["param2"] or 0
+        msg.param3 = params["param3"] or 0
+        msg.param4 = params["param4"] or 0
+        msg.param5 = params["param5"] or 0
+        msg.param6 = params["param6"] or 0
+        msg.param7 = params["param7"] or 0
+        msg.target_system = 1
+        msg.target_component = 1
+        msg.source_system = 1
+        msg.source_component = 1
+        msg.from_external = True
+        msg.timestamp = self.__px4_timestamp_now()
+        self._vehicle_command_pub.publish(msg)
+
+    def __px4_timestamp_now(self) -> int:
+        return self.get_clock().now().nanoseconds / 1000
 
 
 def main(args=None):
