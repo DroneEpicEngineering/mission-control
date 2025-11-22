d9f00ae6e89588df5e5f95110613776c353c775f
karofox <k4aro0@gmail.com>
2025-11-22 12:14:59 +0100

    feat: yaw control for fly commands

--- flight_control/flight_control/coordinate_transforms.py
+++ flight_control/flight_control/coordinate_transforms.py
@@ -1,6 +1,13 @@
+import numpy as np
+
+
 def ned_to_enu(n: float, e: float, d: float) -> tuple[float, float, float]:
     return e, n, -d
 
 
 def enu_to_ned(e: float, n: float, u: float) -> tuple[float, float, float]:
     return n, e, -u
+
+
+def heading_transform(heading: float) -> float:
+    return -heading + (np.pi / 2.0)
