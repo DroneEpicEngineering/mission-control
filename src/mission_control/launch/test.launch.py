3aeeb976ad90e1465663393f490f71e6f9c7f37d
karofox <k4aro0@gmail.com>
2026-01-13 18:07:38 +0100

    feat: add test launch

--- src/mission_control/launch/test.launch.py
+++ src/mission_control/launch/test.launch.py
@@ -9,19 +9,13 @@ def generate_launch_description():
     package_share = get_package_share_path("mission_control")
 
     params_file = LaunchConfiguration("params_file")
-    rviz_config = LaunchConfiguration("rviz_config")
 
     params_file_arg = DeclareLaunchArgument(
         "params_file",
         default_value=(package_share / "params" / "example_params.yaml").as_posix(),
     )
-    rviz_config_arg = DeclareLaunchArgument(
-        "rviz_config",
-        default_value=(package_share / "config" / "visualization.rviz").as_posix(),
-    )
 
     microxrce = ExecuteProcess(cmd=["MicroXRCEAgent", "udp4", "-p", "8888"])
-    tree_viewer = ExecuteProcess(cmd=["py-trees-tree-viewer"])
 
     behaviour_tree = Node(
         executable="behaviour_tree",
@@ -29,28 +23,29 @@ def generate_launch_description():
         parameters=[params_file],
     )
 
-    visualizer = Node(
-        executable="visualizer",
-        package="mission_control",
-        parameters=[params_file],
-    )
-
-    rviz = Node(executable="rviz2", package="rviz2", arguments=["-d", rviz_config])
-
     logger = Node(
         executable="position_logger", package="position_logger", output="screen"
+    )
+
+    autostart = ExecuteProcess(
+        cmd=[
+            "ros2",
+            "topic",
+            "pub",
+            "/start",
+            "std_msgs/msg/Empty",
+            "{}",
+            "--once",
+        ]
     )
 
     ld = LaunchDescription()
 
     ld.add_action(params_file_arg)
-    ld.add_action(rviz_config_arg)
 
     ld.add_action(microxrce)
-    ld.add_action(tree_viewer)
     ld.add_action(behaviour_tree)
-    ld.add_action(visualizer)
-    ld.add_action(rviz)
     ld.add_action(logger)
+    ld.add_action(autostart)
 
     return ld
